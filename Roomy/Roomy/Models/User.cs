9f1420684d278cad9bf5d55cc21d66a3ff979fc8
cyl-dev <cyl-dev@outlook.fr>
2018-07-16 17:02:19 +0200

    Validation

---
+++ Roomy/Roomy/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Le champ Nom est obligatoire")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Le champ {0} doit contenir entre {2} et {1} caractères")]
+        [Display(Name = "Nom")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Prénom")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+@"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+@".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+ErrorMessage = "L'adresse mail n'est pas au bon format")]
+
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+/*        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@% _\w]{8,15})$", ErrorMessage = "Le PW n'est pas au bon format")]*/
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmer le mot de passe")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmedPassword { get; set; }
+        
+    }
+}

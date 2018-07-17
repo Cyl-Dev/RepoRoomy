5af6dd65b4053a0b7f8f7bfb328fb72cc2d2d6a2
cyl-dev <cyl-dev@outlook.fr>
2018-07-17 17:09:12 +0200

    soir

--- Roomy/Roomy/Controllers/UsersController.cs
+++ Roomy/Roomy/Controllers/UsersController.cs
@@ -1,18 +1,48 @@
 ﻿using Roomy.Models;
+using Roomy.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
+
 namespace Roomy.Controllers
 {
     public class UsersController : Controller
     {
+        private RoomyDbContext db = new RoomyDbContext();
+
         // GET: Users
         [HttpGet]
         public ActionResult Create()
         {
+
+            ViewBag.Civilities = db.Civilities.ToList();
+
+
+            /*
+            List<Civility> numQuery2 = db.Civilities.ToList();
+
+            ViewBag.CivilityList = numQuery2;
+            //            ViewBag.CivilityList = new SelectList(Enum.GetValues(typeof(Civility)));
+            */
+
+            /*
+
+            IEnumerable<SelectListItem> items = db.Civilities.Select(c => new SelectListItem
+            {
+                Value = c.ID.ToString(),
+                Text = c.Label
+            });
+            ViewBag.CivilityList = items;
+
+    */
+
+
+
+
             return View();
         }
 
@@ -21,11 +51,13 @@ namespace Roomy.Controllers
         {
             if (ModelState.IsValid)
             {
-
-
+                // enregister
+                db.Users.Add(user);
+                db.SaveChanges();
 
             }
 
+            ViewBag.Civilities = db.Civilities.ToList();
             return View();
         }
 
